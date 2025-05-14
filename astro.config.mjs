245db973f86a11db62c7b45944f0cdddc46370b1
Mike Kobit <mkobit@gmail.com>
2025-05-14 09:22:52 -0500

    Minor configuration updates and checking against github pages domain setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://allbugsarefixed.com',
-  base: '/',
+  site: "https://allbugsarefixed.com",
+  base: "/",
   integrations: [mdx()],
-  trailingSlash: 'always',
+  trailingSlash: "always",
+  output: "static",
 });
