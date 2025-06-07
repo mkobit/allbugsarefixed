b034b59790db0323681141c30b7cc0da49093678
Mike Kobit <mkobit@gmail.com>
2025-06-06 22:15:50 -0500

    Start iterating on styling and prep for first planned post (#3)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-  type: 'content', // v2.5.0 and later
+  type: "content",
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
     tags: z.array(z.string()).optional(),
+    draft: z.boolean().optional().default(false),
   }),
 });
 
 export const collections = {
-  'blog': blogCollection,
+  blog: blogCollection,
 };
