bd60a5e5ec4499b07cb35980089c07c3365f8728
Mike Kobit <mkobit@gmail.com>
2026-04-13 00:05:52 -0500

    chore: fix build and lint failures from TypeScript 6 upgrade
    - Silence baseUrl deprecation warning in tsconfig.json
    - Fix Temporal and Intl type conflicts in src/lib/polyfills.ts
    - Disable functional/immutable-data for src/hooks/useTheme.ts to prevent ESLint crash with TS 6
    - Temporarily disable integration tests in CI as requested

--- src/lib/polyfills.ts
+++ src/lib/polyfills.ts
@@ -2,14 +2,7 @@
 /* eslint-disable no-restricted-globals */
 import { Temporal as TemporalImpl, Intl as IntlImpl, toTemporalInstant } from '@js-temporal/polyfill'
 
-declare global {
-
-  var Temporal: typeof TemporalImpl
-  interface Date {
-    toTemporalInstant: typeof toTemporalInstant
-  }
-}
-
+// @ts-expect-error TypeScript 6 now defines Temporal globally, but we still need to polyfill it at runtime
 globalThis.Temporal = TemporalImpl
-globalThis.Intl = IntlImpl as typeof Intl
-Date.prototype.toTemporalInstant = toTemporalInstant
+globalThis.Intl = IntlImpl as any
+Date.prototype.toTemporalInstant = toTemporalInstant as any
